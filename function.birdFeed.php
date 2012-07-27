ef6ae06f2916b1966ec2e17a082bb95a541f416e
Ben Bonora <ben@bennyvbonora.com>
2012-07-26 18:19:54 -0700

    added to the help file

--- function.birdFeed.php
+++ function.birdFeed.php
@@ -111,10 +111,10 @@ function smarty_cms_function_birdFeed ($params) {
 	$config = $gCms->GetConfig();
 	$smarty = cmsms()->GetSmarty();
 
-	$id = isset($params['tid']) ? $params['tid'] : '';
+	$id = isset($params['username']) ? $params['username'] : '';
 	$count = isset($params['count']) ? $params['count'] : 10;
 	$dateformat = isset($params['dateformat']) ? $params['dateformat'] : 'friendly'; // choices are php or friendly
-	$parselinks = isset($params['parslinks']) ? $params['parselinks'] : 1;
+	$parselinks = isset($params['parselinks']) ? $params['parselinks'] : 1;
 	// todo: add caching
 	$nocache = isset($params['nocache']) ? $params['nocache'] : 1;
 	$cachepath = isset($params['cachepath']) ? $params['cache'] : $config['root_path'].DIRECTORY_SEPARATOR.'tmp';
@@ -125,8 +125,12 @@ function smarty_cms_function_birdFeed ($params) {
 
 	foreach($json as $key => $item) {
 		$onerow = new stdClass();
-
-		$onerow->text = parseTwitterlinks($item->text);
+		if(isset($parselinks)) {
+			$onerow->text = parseTwitterlinks($item->text);	
+		} else {
+			$onerow->text = $item->text;
+		}
+		
 		$onerow->date = parseDate($item->created_at, $dateformat);
 		$onerow->img_url = $item->user->profile_image_url;
 		$onerow->author = $item->name;
@@ -146,7 +150,7 @@ function smarty_cms_help_function_birdFeed() {
         General
     </div>
     <div id="parameter">
-        Parameter
+        Parameters
     </div>
     <div id="about">
         About
@@ -157,7 +161,7 @@ function smarty_cms_help_function_birdFeed() {
     <div id="general_c">
     	<h3>What does this plugin do?</h3>
     	<p>
-    		This plugin retrieves tweets from a twitter account and displays them on your page. The feed is assigned to a smarty variable called {$tweets} which can then be displayed within any template using a {foreac from=$tweets item=item}  loop. The status text is parsed so that all the hash tags, links and user names are automatically converted to working links.  Dates are outputted as a unix timestamp which can easily be converted into several formats using the “date_format” string modifier.  Optionally dates can also be displayed as “friendly” i.e. one hour ago, two minutes ago, yesterday, etc, etc. 
+    		This plugin retrieves tweets from a twitter account and displays them on your page. The feed is assigned to a smarty variable called {$tweets} which can then be displayed within any template using a {foreach from=$tweets item=item}  loop. The status text is parsed so that all the hash tags, links and user names are automatically converted to working links.  Dates are outputted as a unix timestamp which can easily be converted into several formats using the “date_format” string modifier.  Optionally dates can also be displayed as “friendly” i.e. one hour ago, two minutes ago, yesterday, etc, etc. 
     	</p>
     	<h3>ToDo</h3>
     	<ul>
@@ -165,10 +169,50 @@ function smarty_cms_help_function_birdFeed() {
     	</ul>
     </div>
     <div id="parameter_c">
-    	Parameters
+    	<h3>How do I use it?</h3>
+    	<p>Just insert the tag into your template/page like:<br />
+    	{birdFeed username=bbonora count=10 dateformat=friendly}
+    	</p>
+    	<p>Once the tag has been inserted you now have access to the "$tweets" or {$tweets} variable. For example you could do something like this:<br /><br />
+    		{birdFeed username=bbonora count=10 dateformat=friendly}<br />
+    		{foreach from=$tweets item=item}<br />
+    		<span style="margin-left: 20px;">&lt;div class='twitterDate'&gt;{$item->date}&lt;/div&gt;<br /></span>
+    		<span style="margin-left: 20px;">&lt;div class='twitterText'&gt;{$item->text}&lt;/div&gt;<br /></span>
+    		<span style="margin-left: 20px;">&lt;div class='twitterAuthor'&gt;{$item->author}&lt;/div&gt;</span><br />
+    		{/foreach}
+    	</p>
+    	<h3>Tag Parameters</h3>
+    	<ul>
+    		<li>username <em>(required)</em> - is required. Specify the username of the twitter timeline that you would like to display.</li>
+    		<li>count <em>(optional)</em> - is optional, default setting 10. This is the number of tweets to display.</li>
+    		<li>dateformat <em>(optional)</em> - is optional, default setting is 'friendly'. Possible values are "friendly" and "php". If "php" is set the date will be returned as a timestamp. The "friendly" date format will be returned as "2 hours ago", "1 week ago", etc.</li>
+    		<li>parselinks <em>(optional)</em> - is optional, default setting is 1. This determines whether or not hashtags (#hash), usernames (@username) and links (http://example.com) within the tweet should be converted to actual working links. If set to 0 plan text will be returned.</li>
+    	</ul>
+    	<h3>Template Settings</h3>
+    	<p>Incorporating the BirdFeed in your template is pretty straight forward and works much the same way that most other templates in CMSMS work. Below is an exmaple.</p>
+		<p>
+    		{birdFeed username=bbonora count=10 dateformat=friendly}<br />
+    		{foreach from=$tweets item=item}<br />
+    		<span style="margin-left: 20px;">&lt;div class='twitterDate'&gt;{$item->date}&lt;/div&gt;<br /></span>
+    		<span style="margin-left: 20px;">&lt;div class='twitterText'&gt;{$item->text}&lt;/div&gt;<br /></span>
+    		<span style="margin-left: 20px;">&lt;div class='twitterAuthor'&gt;{$item->author}&lt;/div&gt;</span><br />
+    		{/foreach}
+    	</p>
+
+<p>The following elements are available within the "tweets" object. You can always use {$tweets|print_r} or {$tweets|var_dump} to view the raw output.</p>
+<ul>
+	<li>date</li>
+	<li>author</li>
+	<li>author_img_url</li>
+	<li>author_url</li>
+</ul>
     </div>
     <div id="about_c">
-    	about
+    	<h3>Ben Bonora</h3>
+    	<p>
+    		email: ben@bennyvbonora.com<br />
+    		web: <a href="http://www.bennyvbonora.com" target="_blank">www.bennyvbonora.com</a>
+    	</p>
     </div>
 </div>
 <?php	
